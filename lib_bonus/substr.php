c8cad0ac933b632993b0598d55d6a337247b3206
dark-angel-the-one <aruby9905@gmail.com>
2022-04-20 22:54:34 +0200

    commencement de substr

---
+++ lib_bonus/substr.php
@@ -0,0 +1,16 @@
+<?php 
+
+/*  */
+
+include 'explode.php';
+
+function fn_substr($str, $offset, $length=null){
+    
+    for ($i=0; $i < fn_strlen($tab); $i++) { 
+        if ($tab[$i] == fn_strlen($tab)){
+            return $tab[$i];
+        }
+    }
+}
+$str = "coucouçava";
+echo fn_substr($str, -1);
